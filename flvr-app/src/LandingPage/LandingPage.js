bc6c533e27b044d22815bf1dd0c916dbe21644ee
Aldiyar Batyrbekov <aldibatyrbekov@gmail.com>
2019-06-17 15:21:09 -0700

    created folders for files

---
+++ flvr-app/src/LandingPage/LandingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export default class LandingPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        Landing Page Place Holder
+      </div>
+    )
+  }
+}
